4257d4b070b5b4b550e7f79c1f68754a86b3be50
pietro.rocchio <pietro.rocchio@marconirovereto.it>
2018-02-11 22:18:24 +0100

    Add var.js - Tipi di variabili, dichiarazione ed assegnazione.

---
+++ JS Basic/var.js
@@ -0,0 +1,22 @@
+//dichiarazione variabili
+var nomeVariabile;
+var text;
+var booleana;
+var virgola;
+
+//assegnazione valore ad un avariabile
+text = 'ciao';
+nomeVariabile = 23;
+booleana = true;
+virgola = 45.342;
+
+/*
+    COME POTETE NOTARE NON SERVE SPECIFICARE IL TIPO DELLA VARIBILE "INTERA, FLOAT, BOOL, ECC...".
+    --
+    L'ASSEGNAZIONE DI UN VALORE PUO' ESSERE ESEGUITO DIRETTAMENTE DURANTE LA DICHIARAZIONE DI ESSA.
+*/
+
+var numero = 3;
+var parola = "text";
+var numeroConVirgola = 34.3;
+var vero = true;
